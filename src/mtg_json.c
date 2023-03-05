320b0bfa6374717bd4c4aa712fa5d0c173486aac
Danny Piper <djpiper28@gmail.com>
2023-03-05 14:50:58 +0000

    yooo, moving machines - good commit init

--- src/mtg_json.c
+++ src/mtg_json.c
@@ -1,4 +1,30 @@
 #include "./mtg_json.h"
 #include "./io_utils.h"
+#include "../testing_h/testing.h"
+#include <unistd.h>
+#include <curl/curl.h>
 
-mtg_json_query_result_t get_atomic_cards();
+int get_atomic_cards(mtg_json_query_result_t *ret, thread_pool_t *pool)
+{
+  // Create the pipe that will be used for IPC
+  int fid[2];
+  int r = pipe();
+  ASSERT(r == 0);
+
+  FILE *r = fdopen(fid[0], "r");
+  ASSERT(r != NULL):
+
+  FILE *w = fdopen(fid[1], "w");
+  ASSERT(w != NULL);
+
+  // Start curl request that writes to a pipe in another thread
+  
+  // Start reading the json in this thread
+
+  // If the json cannot be read due to an error, then the curl request failed, its thread should have cleared
+  // that bit up
+  
+  fclose(r);
+  
+  return 1;
+}
--- src/mtg_json.h
+++ src/mtg_json.h
@@ -1,12 +1,12 @@
 #pragma once
+#include "./thread_pool.h"
 #include <jansson.h>
 
 typedef struct mtg_json_query_result_t {
-    /// The return code, 200 is probably good
-    int code;
     /// Json body of the query, this is NULL on error
     json_t *json;
 } mtg_json_query_result_t;
 
-mtg_json_query_result_t get_atomic_cards();
+/// Returns 1 on success, 0 on failure
+int get_atomic_cards(mtg_json_query_result_t *ret, thread_pool_t *pool);
 
