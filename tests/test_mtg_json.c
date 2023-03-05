f3ca8fbb3ad2e86ad07e821b3df2d26393be0dbf
Danny Piper <djpiper28@gmail.com>
2023-03-05 16:42:03 +0000

    git add .

---
+++ tests/test_mtg_json.c
@@ -0,0 +1,21 @@
+#include "./test_mtg_json.h"
+#include "../testing_h/testing.h"
+#include "../src/mtg_json.h"
+#include "../src/thread_pool.h"
+
+static int test_init_free()
+{
+    thread_pool_t pool;
+    ASSERT(init_pool(&pool));
+
+    mtg_json_query_result_t ret;
+    ASSERT(get_atomic_cards(&ret, &pool));
+    ASSERT(ret.json != NULL);
+
+    ASSERT(free_pool(&pool));
+    return 1;
+
+    return 1;
+}
+
+SUB_TEST(test_mtg_json, {&test_init_free, "Test init and, free"})
