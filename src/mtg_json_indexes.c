6b27be506a5d19f2dfbecc8bb6cca56fab9c1742
Danny Piper <djpiper28@gmail.com>
2023-03-17 15:59:21 +0000

    fix: compare sets not set codes in tree lookup

--- src/mtg_json_indexes.c
+++ src/mtg_json_indexes.c
@@ -2,29 +2,47 @@
 #include "../testing_h/testing.h"
 #include <string.h>
 
-static void __add_card_to_set(mtg_card_t *card, avl_tree_node *sets)
+static int __add_card_to_set(mtg_card_t *card, avl_tree_node *sets)
 {
     for (size_t i = 0; i < card->set_codes_count; i++) {
-        avl_tree_node *set_node = find_payload(sets, card->set_codes[i]);
-        add_card_to_set((mtg_set_t *) set_node->payload, card);
+        // Create a proxy element for the tree search as the tree will be comparing mtg_set_t objects
+        mtg_set_t card_set_proxy;
+        memcpy(card_set_proxy.code, card->set_codes[i], sizeof(card_set_proxy.code));
+
+        avl_tree_node *set_node = find_payload(sets, &card_set_proxy);
+
+        if (set_node != NULL) {
+            add_card_to_set((mtg_set_t *) set_node->payload, card);
+        } else {
+            char set_code[MAX_SET_CODE_LEN + 1];
+            set_code[MAX_SET_CODE_LEN] = 0;
+            memcpy(set_code, card->set_codes[i], sizeof(card->set_codes[i]));
+
+            lprintf(LOG_ERROR, "Cannot find set with code %s for card %s\n", set_code, card->name);
+            return 0;
+        }
     }
+    return 1;
 }
 
-static void __add_cards_to_set(avl_tree_node *cards, avl_tree_node *sets)
+static int __add_cards_to_set(avl_tree_node *cards, avl_tree_node *sets)
 {
     if (cards == NULL) {
-        return;
+        return 1;
     }
 
-    __add_card_to_set((mtg_card_t *) cards->payload, sets);
-    __add_cards_to_set(cards->l, sets);
-    __add_cards_to_set(cards->r, sets);
+    ASSERT(__add_card_to_set((mtg_card_t *) cards->payload, sets));
+    ASSERT(__add_cards_to_set(cards->l, sets));
+    ASSERT(__add_cards_to_set(cards->r, sets));
+    return 1;
 }
 
 static void __generate_set_cards_index_task(void *__state, thread_pool_t *pool)
 {
     mse_index_generator_state_t *state = (mse_index_generator_state_t *) __state;
-    __add_cards_to_set(state->cards->card_tree, state->cards->set_tree);
+    if (!__add_cards_to_set(state->cards->card_tree, state->cards->set_tree)) {
+    state->ret = 0;
+  }
     sem_post(&(state->semaphore));
 }
 
@@ -38,13 +56,25 @@ int __generate_indexes(mtg_all_printings_cards_t *ret, thread_pool_t *pool)
     void (*tasks[])(void *, struct thread_pool_t *) = {&__generate_set_cards_index_task};
 
     mse_index_generator_state_t state;
+    state.ret = 1;
+    state.cards = ret;
+    state.pool = pool;
     sem_init(&state.semaphore, 0, 0);
+
+    // Start the tasks
+    for (size_t i = 0; i < TASK_COUNT(tasks); i++) {
+        task_t task = {(void *) &state, tasks[i]};
+        ASSERT(task_queue_enqueue(&pool->queue, task));
+    }
 
     // Wait for all of the tasks to complete on the thread pool
     for (size_t i = 0; i < TASK_COUNT(tasks); i++) {
         sem_wait(&state.semaphore);
     }
+    int tmp = state.ret;
     free_index_generator_state(&state);
+
+    ASSERT(tmp);
     return 1;
 }
 
--- src/mtg_json_indexes.h
+++ src/mtg_json_indexes.h
@@ -10,6 +10,7 @@ typedef struct mse_index_generator_state_t {
     mtg_all_printings_cards_t *cards; // owned by the caller
     thread_pool_t *pool; // owned by the caller
     sem_t semaphore;
+    int ret;
 } mse_index_generator_state_t;
 
 void free_index_generator_state(mse_index_generator_state_t *state);
