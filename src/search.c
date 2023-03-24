28a88bed3875027e0f585c5a0783351b84bde8d4
Danny Piper <djpiper28@gmail.com>
2023-03-24 16:07:03 +0000

    feat: added set intersection

--- src/search.c
+++ src/search.c
@@ -20,17 +20,23 @@ mse_search_intermediate_t init_mse_search_intermediate_tree(avl_tree_node_t *nod
     return ret;
 }
 
+static int __copy_node(avl_tree_node_t **node, avl_tree_node_t *node_old)
+{
+    *node = shallow_copy_tree_node(node_old);
+    ASSERT(*node != NULL);
+    (*node)->cmp_payload = COMPARE_FUNCTION;
+    (*node)->free_payload = FREE_FUNCTION;
+    return 1;
+}
+
 static int __insert_tree_set_union(avl_tree_node_t **tree, avl_tree_node_t *node_old)
 {
     if (node_old == NULL) {
         return 1;
     }
 
-    // Shallow copy of the node
-    avl_tree_node_t *node = shallow_copy_tree_node(node_old);
-    ASSERT(node != NULL);
-    node->cmp_payload = COMPARE_FUNCTION;
-    node->free_payload = FREE_FUNCTION;
+    avl_tree_node_t *node = NULL;
+    ASSERT(__copy_node(&node, node_old));
 
     if (!insert_node(tree, node)) {
         free_tree(node);
@@ -59,12 +65,41 @@ int mse_set_union(mse_search_intermediate_t *ret,
     return 1;
 }
 
+static int __mse_set_intersection(avl_tree_node_t **ret,
+                                  avl_tree_node_t *node,
+                                  avl_tree_node_t *tree)
+{
+    if (node == NULL) {
+        return 1;
+    }
+
+    if (find_payload(tree, node->payload)) {
+        avl_tree_node_t *node_copy = NULL;
+        ASSERT(__copy_node(&node_copy, node));
+        ASSERT(insert_node(ret, node_copy));
+    }
+
+    ASSERT(__mse_set_intersection(ret, node->l, tree));
+    ASSERT(__mse_set_intersection(ret, node->r, tree));
+    return 1;
+}
+
 int mse_set_intersection(mse_search_intermediate_t *ret,
                          mse_search_intermediate_t *a,
                          mse_search_intermediate_t *b)
 {
     __init_mse_search_intermediate(ret);
-    return 0;
+    if (a->node == NULL || b->node == NULL) {
+        return 1;
+    }
+
+    // Small performance increase as the intersection by iterating over the smaller set then finding each in the
+    // other is far faster
+    if (a->node->height > b->node->height) {
+        return mse_set_intersection(ret, b, a);
+    }
+
+    return __mse_set_intersection(&ret->node, a->node, b->node);
 }
 
 void free_mse_search_intermediate(mse_search_intermediate_t *inter)
--- tests/test_search.c
+++ tests/test_search.c
@@ -51,4 +51,34 @@ static int test_union_tree()
     return 1;
 }
 
-SUB_TEST(test_search, {&test_union_tree, "Test set union on trees"})
+static int test_intersection_tree()
+{
+    // Init trees
+    avl_tree_node_t *tree_1 = get_tree(1, 100);
+    ASSERT(tree_size(tree_1) == 100);
+    ASSERT(tree_1 != NULL);
+    mse_search_intermediate_t a = init_mse_search_intermediate_tree(tree_1);
+
+    avl_tree_node_t *tree_2 = get_tree(90, 200);
+    ASSERT(tree_size(tree_2) == 111);
+    ASSERT(tree_2 != NULL);
+    mse_search_intermediate_t b = init_mse_search_intermediate_tree(tree_2);
+
+    // Test set operations
+    mse_search_intermediate_t inter;
+    ASSERT(mse_set_intersection(&inter, &a, &b));
+
+    ASSERT(inter.node != NULL);
+    ASSERT(tree_size(inter.node) == 11);
+
+    free_mse_search_intermediate(&inter);
+
+    free_tree(tree_1);
+    free_tree(tree_2);
+
+    return 1;
+
+}
+
+SUB_TEST(test_search, {&test_union_tree, "Test set union on trees"},
+{&test_intersection_tree, "Test set intersection on trees"})
