c32e15b2cd487e286f04bc817e010ba4af6353cc
Danny Piper <djpiper28@gmail.com>
2023-04-07 14:19:02 +0100

    feat: added set lookup
    untested

---
+++ src/generator_set.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "./generators.h"
+#include "./avl_tree.h"
+#include "./mtg_json.h"
+
+int mse_generate_set_set(mse_set_generator_t *gen,
+                         avl_tree_node_t **res,
+                         mtg_all_printings_cards_t *cards);
