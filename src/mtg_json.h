014a5566694fe3550e3dbe1b7eb7992a4b5c4706
Danny Piper <djpiper28@gmail.com>
2023-04-19 15:02:46 +0100

    feat: added partially threaded colour index generator

--- src/mtg_json.h
+++ src/mtg_json.h
@@ -14,6 +14,29 @@ size_t __mse_json_write_callback(char *ptr,
                                  size_t nmemb,
                                  void *data);
 
+#define MSE_COLOUR_INDEX_MAX_CARDS (MSE_WUBRG + 1)
+
+/// The indexes for the cards which are in a large list and sorted by
+/// the bit flags from 00000 to 11111
+typedef struct mse_colour_index_t {
+    avl_tree_node_t * colour_indexes[MSE_COLOUR_INDEX_MAX_CARDS];
+} mse_colour_index_t;
+
+void free_mse_colour_index(mse_colour_index_t *index);
+
+#define MSE_COLOUR_INDEX_GENERATOR(index_base_name) \
+mse_colour_index_t index_base_name##_lt; \
+mse_colour_index_t index_base_name##_lt_inc; \
+mse_colour_index_t index_base_name##_gt; \
+mse_colour_index_t index_base_name##_gt_inc; \
+mse_colour_index_t index_base_name##_eq;
+
+/// Holds all of the colour indexes
+typedef struct mse_cards_colours_indexes_t {
+    MSE_COLOUR_INDEX_GENERATOR(colours);
+    MSE_COLOUR_INDEX_GENERATOR(colour_identity);
+} mse_cards_colours_indexes_t;
+
 /// Contains all of the card indexes, each index will have a payload that is a pointer to a card,
 /// however it will have a NULL free pointer as the memory is owneed by mse_all_printings_cards_t
 /// in card_tree
@@ -28,6 +51,9 @@ typedef struct mse_cards_indexes_t {
     mse_card_trie_node_t *card_name_trie;
     /// Card names parts trie where the full filtered name is used
     mse_card_trie_node_t *card_name_parts_trie;
+
+    /// Colours index, this is a long list of stuff
+    mse_cards_colours_indexes_t colour_index;
 } mse_cards_indexes_t;
 
 /// This will store the atomic cards and, sets. Each card is stored once and,
