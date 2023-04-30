84c7bf91b47fc06db11ec18ef150e8a2d45af99f
Danny Piper <djpiper28@gmail.com>
2023-04-30 15:50:36 +0100

    feat: added test for sub string match

--- tests/test_generator_txt.c
+++ tests/test_generator_txt.c
@@ -7,6 +7,7 @@
 
 #define REGEX_ARG "/.*god of (the )?.*/"
 #define ARG "hAstE"
+#define ARG_LOWER "haste"
 
 static int test_tree_oracle_re(avl_tree_node_t *node)
 {
@@ -55,6 +56,20 @@ static int test_generator_oracle_regex()
     return 1;
 }
 
+static int test_tree_oracle_substr(avl_tree_node_t *node)
+{
+    if (node == NULL) {
+        return 1;
+    }
+
+    mse_card_t *card = (mse_card_t *) node->payload;
+    ASSERT(mse_str_match(card->oracle_text_lower, ARG_LOWER));
+
+    ASSERT(test_tree_oracle_substr(node->l));
+    ASSERT(test_tree_oracle_substr(node->r));
+    return 1;
+}
+
 static int test_generator_oracle_substr()
 {
     mse_set_generator_type_t gen_type = MSE_SET_GENERATOR_ORACLE_TEXT;
@@ -74,7 +89,7 @@ static int test_generator_oracle_substr()
     root = NULL;
     ASSERT(mse_generate_set(&ret, &root, &gen_cards, &gen_thread_pool));
     ASSERT(tree_size(root) > 0);
-    ASSERT(test_tree_oracle_re(root));
+    ASSERT(test_tree_oracle_substr(root));
     free_tree(root);
     mse_free_set_generator(&ret);
     return 1;
