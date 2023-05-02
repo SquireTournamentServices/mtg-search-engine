3a68ea2efd06f729b3f108bbd11fc8afa16206e9
Danny Piper <djpiper28@gmail.com>
2023-05-02 15:39:28 +0100

    feat: test the resolve method

--- tests/test_interpretor.c
+++ tests/test_interpretor.c
@@ -3,6 +3,46 @@
 #include "../src/interpretor.h"
 #include <string.h>
 
+// Some vile testing globals
+static mse_all_printings_cards_t test_cards;
+static thread_pool_t pool;
+
+static json_t *get_all_printings_cards_from_file()
+{
+    FILE *f = fopen("./AllPrintings.json", "rb");
+    json_error_t error;
+    json_t *ret = json_loadf(f, 0, &error);
+
+    if (ret == NULL) {
+        lprintf(LOG_ERROR, "Error: %100s\n", error.text);
+    }
+    fclose(f);
+    return ret;
+}
+
+static int init_test_cards()
+{
+    ASSERT(init_pool(&pool));
+
+    json_t *json = get_all_printings_cards_from_file();
+    ASSERT(json != NULL);
+
+    memset(&test_cards, 0, sizeof(test_cards));
+    ASSERT(__parse_all_printings_cards(&test_cards, json, &pool));
+    json_decref(json);
+
+    ASSERT(test_cards.card_tree != NULL);
+
+    return 1;
+}
+
+static int free_test_card()
+{
+    free_all_printings_cards(&test_cards);
+    ASSERT(free_pool(&pool));
+    return 1;
+}
+
 static int __test_init_free_operator(mse_set_operator_type_t op_type)
 {
     mse_interp_node_t *node = NULL;
@@ -61,6 +101,73 @@ static int test_recursive_free()
     return 1;
 }
 
+#define ARG "M20"
+
+static int test_resolve_set_generator()
+{
+    mse_set_generator_t generator;
+    ASSERT(mse_init_set_generator(&generator, MSE_SET_GENERATOR_SET, MSE_SET_GENERATOR_OP_EQUALS, ARG, strlen(ARG)));
+
+    mse_interp_node_t *node;
+    ASSERT(node = mse_init_interp_node_generator(generator));
+
+    mse_search_intermediate_t ret;
+    ASSERT(mse_resolve_interp_tree(node, &ret, &pool, 0, &test_cards));
+    ASSERT(ret.node != NULL);
+    ASSERT(tree_size(ret.node) >= 329);
+    free_mse_search_intermediate(&ret);
+
+    ret.node = NULL;
+    ASSERT(mse_resolve_interp_tree(node, &ret, &pool, 1, &test_cards));
+    ASSERT(ret.node == NULL);
+
+    // Cleanup
+    mse_free_interp_node(node);
+    return 1;
+}
+
+#define REGEX_1 ".*\\d2.*"
+#define REGEX_2 ".*(\\{[wubrg]\\}){2}.*"
+
+static int test_resolve_tree_1()
+{
+    mse_interp_node_t *root;
+    ASSERT(root = mse_init_interp_node_operation(MSE_SET_UNION));
+
+    mse_set_generator_t generator;
+    ASSERT(mse_init_set_generator(&generator,
+                                  MSE_SET_GENERATOR_ORACLE_TEXT,
+                                  MSE_SET_GENERATOR_OP_EQUALS,
+                                  REGEX_1,
+                                  strlen(REGEX_1)));
+    ASSERT(root->l = mse_init_interp_node_generator(generator));
+
+    ASSERT(mse_init_set_generator(&generator,
+                                  MSE_SET_GENERATOR_ORACLE_TEXT,
+                                  MSE_SET_GENERATOR_OP_EQUALS,
+                                  REGEX_2,
+                                  strlen(REGEX_2)));
+    ASSERT(root->r = mse_init_interp_node_generator(generator));
+
+    mse_search_intermediate_t ret;
+    ASSERT(mse_resolve_interp_tree(root, &ret, &pool, 0, &test_cards));
+    ASSERT(ret.node != NULL);
+    ASSERT(tree_size(ret.node) >= 1);
+    free_mse_search_intermediate(&ret);
+
+    ret.node = NULL;
+    ASSERT(mse_resolve_interp_tree(root, &ret, &pool, 1, &test_cards));
+    ASSERT(ret.node== NULL);
+
+    // Cleanup
+    mse_free_interp_node(root);
+    return 1;
+}
+
 SUB_TEST(test_interpretor, {&test_init_free_operator, "Test init free for operator node"},
 {&test_init_free_generator, "Test init free for generator node"},
-{&test_recursive_free, "Test recursive free"})
+{&test_recursive_free, "Test recursive free"},
+{&init_test_cards, "Init test cards"},
+{&test_resolve_set_generator, "Test resolve generator"},
+{&test_resolve_tree_1, "Test resolve tree 1"},
+{&free_test_card, "Free test cards"})
