105baf3660d96f4fd3af6b32b17eab370f834a27
Danny Piper <djpiper28@gmail.com>
2023-06-07 16:24:43 +0100

    feat: update format script

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find ./src ./tests* | grep ".*\\.[ch]\$" | xargs astyle --style=kr
-find ./src ./tests* | grep ".*\\.cpp\$" | xargs astyle --style=kr
+find ./mse ./tests* | grep ".*\\.[ch]\$" | xargs astyle --style=kr
+find ./mse ./tests* | grep ".*\\.cpp\$" | xargs astyle --style=kr
 cmake-format ./CMakeLists.txt > ./CMakeLists.txt.orig && cp ./CMakeLists.txt.orig ./CMakeLists.txt
 black *.py
 
