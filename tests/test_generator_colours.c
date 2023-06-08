4b039725175a34f8bdc0d01e0c1481bb733ab995
Danny Piper <djpiper28@gmail.com>
2023-06-08 16:11:32 +0100

    chore: reformat

--- tests/test_generator_colours.c
+++ tests/test_generator_colours.c
@@ -101,11 +101,11 @@ SUB_TEST(test_generator_colours, {&test_generator_colours_lt, "Colours generator
 {&test_generator_colours_lt_inc, "Colours generator <="},
 {&test_generator_colours_gt, "Colours generator >"},
 {&test_generator_colours_gt_inc, "Colours generator >="},
-{&test_generator_colours_eq, "Colours generator ==",},
+{&test_generator_colours_eq, "Colours generator =="},
 {&test_generator_colours_inc, "Colours generator == (inc)"},
 {&test_generator_colour_identity_lt, "Colour identity generator <="},
 {&test_generator_colour_identity_lt_inc, "Colour identity generator <="},
 {&test_generator_colour_identity_gt, "Colour identity generator >"},
 {&test_generator_colour_identity_gt_inc, "Colour identity generator >="},
-{&test_generator_colour_identity_eq, "Colour identity generator ==",},
+{&test_generator_colour_identity_eq, "Colour identity generator =="},
 {&test_generator_colour_identity_inc, "Colour identity generator == (inc)"})
