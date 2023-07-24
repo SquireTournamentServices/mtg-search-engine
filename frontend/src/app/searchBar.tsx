2bf555583de0ef93721d09b5b24d06fced703058
Danny Piper <danny.piper@bjss.com>
2023-07-24 13:27:03 +0100

    feat: added search bar

---
+++ frontend/src/app/searchBar.tsx
@@ -0,0 +1,6 @@
+export default function SearchEngine() {
+    return <form>
+        <input type="text" required placeholder="Search"></input>
+        <button type="submit">Search</button>
+    </form>
+}
