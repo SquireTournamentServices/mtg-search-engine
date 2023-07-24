756987b6f1e7b26169dd74a4465b9c51a0d790ce
Danny Piper <danny.piper@bjss.com>
2023-07-24 15:45:33 +0100

    fix: search bar now works

--- frontend/src/app/searchBar.tsx
+++ frontend/src/app/searchBar.tsx
@@ -1,13 +1,12 @@
 'use client'
-import { useEffect, useState } from "react"
-import Router from "next/router";
+import { useState } from "react"
 
 export default function SearchBar(props) {
     const [name, setName] = useState(props.query || "");
 
     return (
     <div className="flex flex-row bg-slate-100 rounded-l px-3">
-      <form onSubmit={() => {window.location.href = "/" + encodeURIComponent(name);}}>
+      <form onSubmit={(e) => {window.location.href = "/" + encodeURIComponent(name); e.preventDefault();}}>
         <input type="text" required placeholder="Search" value={name} onChange={(e) => setName(e.target.value)} className="w-max bg-slate-100" />
         <button type="submit" className="px-5 hover:bg-slate-200 rounded">Search</button>
       </form>
