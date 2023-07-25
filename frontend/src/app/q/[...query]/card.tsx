0bc2fe47077a4c8ce5cc5dc23d080c7f78e0186f
Danny Piper <danny.piper@bjss.com>
2023-07-25 16:26:29 +0100

    feat: added coiour identity indicator

--- frontend/src/app/q/[...query]/card.tsx
+++ frontend/src/app/q/[...query]/card.tsx
@@ -1,13 +1,22 @@
 import { isPropertySignature } from "typescript";
 import Manamoji from "./manamoji.tsx";
 import Oracle from "./oracle.tsx";
+import Colour from "./colour.tsx";
 
 export default function Card(props) {
+  const colours = [];
+  for (var i = 1; i < 1 << 5; i <<= 1) {
+    const c = props.data.colour_identity & i;
+    if (c) {
+      colours.push(c);
+    }
+  }
+
   return (
     <div className="flex flex-col rounded-xl bg-slate-100 w-72 min-h-[300px] p-5 drop-shadow">
-      <div className="flex flex-row flex-wrap gap-3 justify-between">
+      <div className="flex flex-row gap-3 justify-between">
         <h2 className="font-bold">{props.data.name}</h2>
-        <div className="flex flex-row flex-wrap w-max">
+        <div className="flex flex-row flex-wrap w-max justify-end">
           {props.data.mana_cost &&
             props.data.mana_cost
               .split("{")
@@ -22,11 +31,18 @@ export default function Card(props) {
         <Oracle oracle_text={props.data.oracle_text} />
       )}
       <div className="grow" />
-      <p className="text-right">
-        {props.data.power && props.data.toughness
-          ? props.data.power + "/" + props.data.toughness
-          : ""}
-      </p>
+      <div className="flex flex-row justify-between">
+        <span>
+          {colours.map((c) => (
+            <Colour colour={c} />
+          ))}
+        </span>
+        <p className="text-right">
+          {props.data.power && props.data.toughness
+            ? props.data.power + "/" + props.data.toughness
+            : ""}
+        </p>
+      </div>
     </div>
   );
 }
