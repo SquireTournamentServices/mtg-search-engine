bf21f7448a33a074c1f9dec7cda7e5d7902d72f6
Danny Piper <danny.piper@bjss.com>
2023-07-25 10:48:33 +0100

    chore: move the query page to /q/[...query]

--- frontend/src/app/q/[...query]/page.tsx
+++ frontend/src/app/q/[...query]/page.tsx
@@ -1,4 +1,4 @@
-import SearchBar from "../searchBar";
+import SearchBar from "../../searchBar";
 import Card from "./card";
 
 export default async function SearchResultPage({ params }) {
--- frontend/src/app/searchBar.tsx
+++ frontend/src/app/searchBar.tsx
@@ -1,33 +1,31 @@
 "use client";
 import { useState } from "react";
-import Link from 'next/link';
 
 export default function SearchBar(props) {
   const [name, setName] = useState(props.query || "");
 
   return (
     <div className="flex flex-row">
-      <h1 className="px-10"><Link href="/">MTG Search engine</Link></h1>
-    <div className="flex flex-row bg-slate-100 rounded-xl px-3 py-1">
-      <form
-        onSubmit={(e) => {
-          window.location.href = "/" + encodeURIComponent(name);
-          e.preventDefault();
-        }}
-      >
-        <input
-          type="text"
-          required
-          placeholder="Search"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="w-max bg-slate-100"
-        />
-        <button type="submit" className="hover:bg-slate-200 rounded-xl">
-          🔍
-        </button>
-      </form>
-    </div>
+      <div className="flex flex-row bg-slate-100 rounded-xl px-3 py-1">
+        <form
+          onSubmit={(e) => {
+            window.location.href = "/q/" + encodeURIComponent(name);
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            required
+            placeholder="Search"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-max bg-slate-100 rounded-l"
+          />
+          <button type="submit" className="hover:bg-slate-200 rounded-xl">
+            🔍
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
