5e42ba05c923fb921e7c489a921260d5bc04d10a
Danny Piper <danny.piper@bjss.com>
2023-07-25 10:48:08 +0100

    feat: started work on front page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,16 @@
 import SearchBar from "./searchBar";
+import RandomQuery from "./randomQuery";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <SearchBar />
-      <h1>A FOSS Search Engine For MTG.</h1>
+      <div className="flex flex-col w-full h-full justify-center p-24">
+        <h1 className="text-center text-xl font-bold">
+          A FOSS Search Engine For MTG.
+        </h1>
+        <RandomQuery />
+      </div>
     </main>
   );
 }
---
+++ frontend/src/app/randomQuery.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState } from "react";
+
+export default function RandomQuery() {
+  const [query, setQuery] = useState("");
+  const queries: string[] = [
+    "c:r and name:goblin",
+    "c:u and name:wizard and oracle:/draw( a)|[0-9]cards?/",
+    "goblin motivator",
+    "set:m20 and c:u",
+  ];
+  setInterval(() => {
+    setQuery(queries[Math.floor(Math.random() * queries.length)]);
+  }, 1000);
+
+  return (
+    <div className="flex flex-row">
+      <h2 className="text-l px-3">Powerful search engine, search for anything:</h2>
+      <p>🔍 {query}</p>
+    </div>
+  );
+}
