0798a529fd506b3801e985f407bbd2d3593d8504
Danny Piper <danny.piper@bjss.com>
2023-08-29 12:23:46 +0100

    feat: added page changed to frontend

--- frontend/src/app/q/[...query]/page.tsx
+++ frontend/src/app/q/[...query]/page.tsx
@@ -1,16 +1,23 @@
 import Card from "./card";
+import PageChanger from "./pageChanger";
+import { useRouter } from "next/navigation";
 
 export default async function SearchResultPage({
   params,
 }: {
   params: {
     query: string;
+    page_number?: number;
   };
 }) {
+  const router = useRouter();
   const query = decodeURIComponent(params.query);
   const resp = await fetch("http://127.0.0.1:4365/api", {
     method: "POST",
     body: query,
+    headers: {
+      page: router.query["page"] || 0
+    }
   });
   const data = await resp.json();
   const results = data.cards_total;
@@ -26,8 +33,16 @@ export default async function SearchResultPage({
         ))}
       </div>
       <p className="text-center">
-        {results} Results | Page {page} / {page_size}
+        {results} Results | Page {page} / {pages}
       </p>
+      <div className="flex flex-row gap-3 w-full overflow-x-auto py-5">
+        {Array(pages)
+          .fill(0)
+          .slice(Math.max(0, page - 5), page + 1 + 50)
+          .map((v, i) => (
+            <PageChanger base_url={"/q/" + params.query} page={i + 1} key={i} />
+          ))}
+      </div>
     </div>
   );
 }
--- frontend/src/app/searchBar.tsx
+++ frontend/src/app/searchBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 export default function SearchBar(props: { query?: string }) {
   const [name, setName] = useState(props.query || "");
