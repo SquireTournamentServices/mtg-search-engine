cf235680ef0de207e764f5d99ee3e5aecdf55817
Danny Piper <djpiper28@gmail.com>
2023-11-26 18:22:18 +0000

    fix: page numbers are now correct

--- frontend/src/app/q/page.tsx
+++ frontend/src/app/q/page.tsx
@@ -24,27 +24,29 @@ export default async function SearchResultPage({
   const pages = Math.ceil(results / page_size);
 
   return (
-    <div className="flex flex-col gap-5 pb-5 pt-5">
+    <div className="flex flex-col gap-5 pb-5 pt-5 justify-between">
       <div className="w-full mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         {data.cards.map((item: any) => (
           <Card data={item} key={item} />
         ))}
       </div>
-      <p className="text-center">
-        {results} Results | Page {page} / {pages}
-      </p>
-      <div className="flex flex-row gap-3 w-full overflow-x-auto py-5">
-        {Array(pages)
-          .fill(0)
-          .slice(Math.max(0, page - 5), page + 1 + 50)
-          .map((_, i) => (
-            <PageChanger
-              base_url={"/q/?query=" + searchParams.query}
-              page={i + 1}
-              key={i}
-              currentPage={(i + 1).toString() == searchParams.page}
-            />
-          ))}
+      <div className="flex flex-col">
+        <p className="text-center">
+          {results} Results | Page {page + 1} / {pages}
+        </p>
+        <div className="flex flex-row gap-3 w-full overflow-x-auto py-5">
+          {Array(pages)
+            .fill(0)
+            .slice(Math.max(0, page - 5), page + 1 + 50)
+            .map((_, i) => (
+              <PageChanger
+                base_url={"/q/?query=" + searchParams.query}
+                page={i + 1}
+                key={i}
+                currentPage={(i + 1).toString() == searchParams.page}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
