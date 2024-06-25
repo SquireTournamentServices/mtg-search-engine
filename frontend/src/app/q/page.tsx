7a6b13d1822f667b4ce4907bdcb3e0d6bcf4d8a0
Danny Piper <djpiper28@gmail.com>
2024-06-25 14:41:19 +0100

    Revert "feat: prefetch the data for the next page"
    This reverts commit b7019b35de949fec1bf72e7e21ef851671419d67.

--- frontend/src/app/q/page.tsx
+++ frontend/src/app/q/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useRouter } from "next/router";
 import { searchUrlFor } from "../searchBar";
 import Card from "./card";
 import PageChanger from "./pageChanger";
@@ -13,16 +11,13 @@ export default async function SearchResultPage({
   };
 }) {
   const query = decodeURIComponent(searchParams.query);
-  const resp = await fetch(
-    process.env.BACKEND_URL ?? "http://127.0.0.1:4365/api",
-    {
-      method: "POST",
-      body: query,
-      headers: {
-        page: (parseInt(searchParams.page ?? "1") - 1).toString(),
-      },
+  const resp = await fetch(process.env.BACKEND_URL ?? "http://127.0.0.1:4365/api", {
+    method: "POST",
+    body: query,
+    headers: {
+      page: (parseInt(searchParams.page ?? "1") - 1).toString(),
     },
-  );
+  });
 
   let data: any;
   if (resp.ok) {
@@ -34,8 +29,6 @@ export default async function SearchResultPage({
   const page = data.page;
   const page_size = data.page_size;
   const pages = Math.ceil(results / page_size);
-  const router = useRouter();
-  router.prefetch(searchUrlFor(searchParams.query, page + 1));
 
   return (
     <div className="flex flex-col gap-5 pb-5 pt-5 justify-between w-full">
