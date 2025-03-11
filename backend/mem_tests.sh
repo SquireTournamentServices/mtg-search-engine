6a21362aba98e77fa998876c9dc65a4b3b77720d
Danny Piper <djpiper28@gmail.com>
2025-03-11 17:21:03 +0000

    fix: typo in script

--- backend/mem_tests.sh
+++ backend/mem_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./mtg-search-engine-tests help | grep "|" | grep -v "Arg | Description" | xargs -n1 python3 mem_tests.py
+./mtg-search-engine-tests help | grep "|" | grep -v "Arg | Description" | grep -Eo "[0-9]+ |" | grep -Eo '[0-9]+' | xargs -n1 python3 mem_tests.py
 
